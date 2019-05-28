269964a943640bbe22864b135b4a0825ba489b88
James Raphael Vibar <vibarjamess@gmail.com>
2019-05-28 22:29:20 +0800

    Add core database login

--- app/models/Post.php
+++ app/models/Post.php
@@ -7,4 +7,11 @@ class Post
   {
     $this->db = new Database;
   }
+
+  public function getPosts()
+  {
+    $this->db->query("SELECT * FROM post");
+
+    return $this->db->resultSet();
+  }
 }
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,7 +1,10 @@
 <?php require APP_ROOT . '/views/inc/header.php'; ?>
 <h1><?php echo $data['title'] ?></h1>
 
-<?php echo APP_ROOT; ?>
-<?php echo URL_ROOT; ?>
+<ul>
+  <?php foreach ($data['posts'] as $post) : ?>
+    <li><?php echo $post->name; ?></li>
+  <?php endforeach; ?>
+</ul>
 
 <?php require APP_ROOT . '/views/inc/footer.php'; ?>
